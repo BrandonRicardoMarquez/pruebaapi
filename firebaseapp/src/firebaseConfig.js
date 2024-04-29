48f24c458215cac227675d42185baf648cd8a748
BrandonRicardoMarquez <ing.brandon.marquez@gmail.com>
2024-04-29 11:11:56 -0600

    firebase

---
+++ firebaseapp/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeRD6iw5IL--8dSVT_qqZMYUiAJuU5D0Q",
+  authDomain: "vue-3-2024-8e183.firebaseapp.com",
+  projectId: "vue-3-2024-8e183",
+  storageBucket: "vue-3-2024-8e183.appspot.com",
+  messagingSenderId: "159964422242",
+  appId: "1:159964422242:web:de216e9dd717c7ad0a713b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
